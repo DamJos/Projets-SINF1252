ef48c857f5ff0f29f50d5b44e1c8a4228078482b
DamJos <damiano-joseph.siciliano@student.uclouvain.be>
2016-03-07 21:11:08 +0100

    Update mymallocfree.c

--- mymallocfree.c
+++ mymallocfree.c
@@ -2,7 +2,12 @@
 #include <stdio.h>
 #include "mymallocfree.h"
 
-void mymalloc(){
+void mymalloc(size_t size){
 }
-void myfree(){
+void myfree(void *ptr){
 }
+void mycalloc(size_t size){
+}
+/*int main(int argc, const char *argv[]) { 
+        return 0;
+}*/
