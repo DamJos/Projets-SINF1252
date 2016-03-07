f59e7283d870cbf4be368a5ce150f7a376ff3c4c
DamJos <damiano-joseph.siciliano@student.uclouvain.be>
2016-03-07 21:11:28 +0100

    Update mymallocfree.h

--- mymallocfree.h
+++ mymallocfree.h
@@ -5,6 +5,7 @@ struct bloc_header {
 		     zero : 2,
 		     alloc : 1;
 }
-void mymalloc();
-void myfree();
+void mymalloc(size_t size);
+void myfree(void *ptr);
+void mycalloc(size_t size);
 #endif
