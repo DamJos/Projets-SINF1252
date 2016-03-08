4077ff0848c69797ed2eabfdde57f09fb3da1957
darkyne <gregory.creupelandt@student.uclouvain.be>
2016-03-08 18:11:27 +0100

    Update mymallocfree.c

--- mymallocfree.c
+++ mymallocfree.c
@@ -1,26 +1,10 @@
-#include <stdlib.h>
+/* Prototype de myMalloc
+* Gregory Creupelandt
+*/
+
 #include <stdio.h>
 #include <unistd.h>
-#include "mymallocfree.h"
-
-// Fonction experimentale qui agrandit le heap a chaque appel
-void *mymalloc(size_t t)
-{
-    if (t<=0 || t>536870911) return NULL;
-
-    size_t paddingSize = t%4;
-    size_t headerSize = 4;
-    size_t realSize = t + paddingSize + headerSize;
-    int *headerPointer;
-    int header = createHeader(realSize);
-    void *ptr;
-    headerPointer = sbrk(0);
-    if (sbrk(t+paddingSize)==NULL) return NULL; //il essaye d'agrandir la place dispo, si ça renvoit null alors on arrete ici. Autrement, on continue.
-    ptr = headerPointer+4;
-    *headerPointer = header;
-    return ptr;
-}
-
+#include <string.h>
 
 //Cree une entete dont les 29 premiers bits sont la realSize et les 3 derniers sont 001 car on appelle cette fonction quand on alloue ave mymalloc
 int createHeader(size_t s)
@@ -32,24 +16,26 @@ int createHeader(size_t s)
 
 }
 
-
-
-
-
-
-
-
-
-// Main permettant de tester et compiler
-int main(int argc, const char *argv[])
+// Fonction experimentale qui agrandit le heap a chaque appel
+void *mymalloc(size_t t)
 {
-    int a;
-    a = createHeader(550);
-    printf("%d", a);
-    return 0;
+    if (t<=0 || t>536870911) return NULL;
+
+    size_t paddingSize = t%4;
+    if (paddingSize != 0) paddingSize=4-paddingSize;
+    size_t headerSize = 4;
+    size_t realSize = t + paddingSize + headerSize;
+    int *headerPointer;
+    int header = createHeader(realSize);
+    void *ptr;
+    headerPointer = sbrk(0);
+    if (sbrk(realSize)==NULL) return NULL; //il essaye d'agrandir la place dispo, si ça renvoit null alors on arrete ici. Autrement, on continue.
+    ptr = headerPointer+4;
+    *headerPointer = header;
+    return ptr;
 }
 
-/*
+
 
 void *mycalloc(size_t t)
 {
@@ -65,9 +51,27 @@ void *mycalloc(size_t t)
     if (sbrk(t+paddingSize)==NULL) return NULL; //il essaye d'agrandir la place dispo, si ça renvoit null alors on arrete ici. Autrement, on continue.
     ptr = headerPointer+4;
     *headerPointer = header;
-    *ptr=NULL;
+    memset(ptr,0, (realSize-4));
     return ptr;
 }
 
+void myfree(void *ptr)
+{
+ *((&ptr)-4)=*((&ptr)-4)-1;
+	
+	
+}
 
-*/
+
+// Main permettant de tester et compiler
+int main(int argc, const char *argv[])
+{
+ int *a = (int *) mycalloc(sizeof(int)*2);
+ printf("%d", *a);
+ int *b=(&a)-4;
+ printf("%d",*b);
+ 
+ myfree(a);
+ 
+ printf("%d",*b);
+}
